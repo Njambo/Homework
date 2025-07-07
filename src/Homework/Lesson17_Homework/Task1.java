6e8d8068807db1b368083baf518b4419e7aa6d70
Nikita <nikita.bryakotnin@mail.ru>
2025-07-07 14:58:15 +0300

    StringBuilder

---
+++ src/Homework/Lesson17_Homework/Task1.java
@@ -0,0 +1,31 @@
+package Homework.Lesson17_Homework;
+
+public class Task1 {
+    public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2) {
+        boolean result = true;
+        if (sb1.length() == sb2.length()) {
+            for (int i = 0; i < sb1.length(); i++) {
+                if (sb1.charAt(i) != sb2.charAt(i)) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        else {
+            result = false;
+        }
+        return result;
+    }
+}
+class Task1Test {
+    public static void main(String[] args) {
+        StringBuilder sb3 = new StringBuilder("Hello");
+        StringBuilder sb4 = new StringBuilder("Hello");
+        StringBuilder sb5 = new StringBuilder("hello");
+        boolean a = Task1.ravenstvo(sb3, sb4);
+        System.out.println(a);
+        System.out.println(Task1.ravenstvo(sb4, sb5));
+        System.out.println(Task1.ravenstvo(new StringBuilder(""), new StringBuilder("")));
+        System.out.println(Task1.ravenstvo(new StringBuilder(), new StringBuilder()));
+    }
+}
