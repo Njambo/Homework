50c9a3082611c400bf7144114458a9fa31215b25
Nikita <nikita.bryakotnin@mail.ru>
2025-07-18 12:11:13 +0300

    Параметры метода типа varargs. Аргументы типа command line. Цикл «forea

---
+++ src/Homework/Lesson19_Homework/Task.java
@@ -0,0 +1,38 @@
+package Homework.Lesson19_Homework;
+
+public class Task {
+    public static String[] abc(String[]... array1) {
+        int length = 0;
+        for (String[] array2 : array1) {
+            if (array2 != null) {
+                length += array2.length;
+            }
+        }
+
+        String[] target = new String[length];
+        int count = 0;
+        for (String[] array2 : array1) {
+            for (String s : array2) {
+                target[count] = s;
+                count++;
+            }
+
+        }
+
+        return target;
+    }
+
+    public static void main(String[] args) {
+        String[] target = abc(new String[]{"ok", "hi", "bye"}, new String[]{"ok", "hello", "goodbye"});
+        for (String s : args) {
+            for (int i = 0; i < target.length; i++) {
+                if (s.equals(target[i])) {
+                    target[i] = null;
+                }
+            }
+        }
+        for (String s : target) {
+            System.out.print(s + " ");
+        }
+    }
+}
